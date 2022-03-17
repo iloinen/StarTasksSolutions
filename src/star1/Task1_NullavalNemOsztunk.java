0579c9aed2126ea8b694d83d4846bcfb97afc663
Ria <benedek.maria88@gmail.com>
2022-03-17 19:51:47 +0100

    solution converted into methods

--- src/star1/Task1_NullavalNemOsztunk.java
+++ src/star1/Task1_NullavalNemOsztunk.java
@@ -3,6 +3,8 @@ package star1;
 public class Task1_NullavalNemOsztunk {
 
     public static void main(String[] args) {
+        /* eredeti megoldás:
+
         int first = 10;
         int second = 0;
 
@@ -10,6 +12,40 @@ public class Task1_NullavalNemOsztunk {
             System.out.println(first / second);
         } else {
             System.out.println("Nullával nem osztunk!");
+        }
+        */
+
+        // metódusok hívása:
+
+        System.out.println("divideNumbersAndPrint nevű metódussal:");
+        divideNumbersAndPrint(10, 2);       // kiírja: 5
+        divideNumbersAndPrint(10, 0);       // kiírja: Nullával nem osztunk!
+
+        System.out.println();
+        System.out.println("divideNumbers nevű metódussal:");
+
+        String result1 = divideNumbers(10, 2);      // értéke: "5"
+        String result2 = divideNumbers(10, 0);      // értéke: "Nullával nem osztunk!"
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+    // megoldás void metódussal
+    public static void divideNumbersAndPrint(int first, int second) {
+        if (second != 0) {
+            System.out.println(first / second);
+        } else {
+            System.out.println("Nullával nem osztunk!");
+        }
+    }
+
+    // megoldás nem-void metódussal - az eredmény szöveg lesz
+    public static String divideNumbers(int first, int second) {
+        if (second != 0) {
+            return "" + (first / second);
+        } else {
+            return "Nullával nem osztunk!";
         }
     }
 
